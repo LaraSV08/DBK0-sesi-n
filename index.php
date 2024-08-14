0b8f30067b1afc077d69a228a650a4cbe3ea636b
Franz Alexander Lara Arevalo <ia.franzlara@ufg.edu.sv>
2024-08-13 19:59:57 -0600

    Creación de index.php con estructura html

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>FRANZ ALEXANDER LARA AREVALO</title>
+        <link rel="stylesheet" href="css/bootstrap.min.css" />
+        <link rel="stylesheet" href="css/style.css" />
+        <script src="js/jquery-3.7.1.min.js"></script>
+        <script src="js/booststrap.bundle.min.js"></script>
+    </head>
+    <body>
+
+    </body>
+</html>
